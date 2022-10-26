f72240c684e020ca8073f1f55885cd8e2caf3459
NyxHexen <jd.davis948@gmail.com>
2022-10-26 21:21:48 +0100

    Added functionality for adding new tasks to categories and update db

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+// Sidenav init
+document.addEventListener('DOMContentLoaded', function () {
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+
+  //Datepicker init
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
   });
+
+  // Select init
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
